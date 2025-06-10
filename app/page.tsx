b2efb065854b2ee4230908df0d9726473ffa427d
kshitijcx <kshitij.cx@gmail.com>
2025-06-10 22:49:17 +0530

    add fetch logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 "use client";
 import { supabase } from "@/supabase-client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+interface Task {
+  id: number;
+  title: string;
+  description: string;
+  created_at: string;
+}
 
 const Home = () => {
   const [task, setTask] = useState({ title: "", description: "" });
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
@@ -11,13 +19,36 @@ const Home = () => {
     const { error } = await supabase.from("tasks").insert(task).single(); //can insert one or multiple items using array of objects
     if (error) {
       console.error("Error adding task", error.message);
+      return;
     }
     setTask({ title: "", description: "" });
   };
 
+  const fetchTasks = async () => {
+    const { error, data } = await supabase
+      .from("tasks")
+      .select("*")
+      .order("created_at", { ascending: true });
+
+    if (error) {
+      console.error("Error adding task", error.message);
+      return;
+    }
+    setTasks(data);
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  console.log(tasks);
+
   return (
     <div className="max-w-xl mx-auto mt-30">
-      <form onSubmit={handleSubmit} className="flex flex-col p-5 border rounded-xl gap-10">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col p-5 border rounded-xl gap-10"
+      >
         <input
           type="text"
           placeholder="title"
@@ -31,8 +62,34 @@ const Home = () => {
             setTask((prev) => ({ ...prev, description: e.target.value }))
           }
         />
-        <button type="submit" className="cursor-pointer bg-gray-400 py-2 rounded-xl text-black">Add Task</button>
+        <button
+          type="submit"
+          className="cursor-pointer bg-gray-400 py-2 rounded-xl text-black"
+        >
+          Add Task
+        </button>
       </form>
+
+      <div>
+        <ul>
+          {tasks.map((item, index) => (
+            <li key={index}>
+              <div>
+                <h3>Title</h3>
+                <p>{item.title}</p>
+              </div>
+              <div>
+                <h3>Description</h3>
+                <p>{item.description}</p>
+              </div>
+              <div>
+                <button>Edit</button>
+                <button>Delete</button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
